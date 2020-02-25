477ee7941a38dc25e4e0e9b75fae57d97f2f8d81
crypt0box <pray.happy76@gmail.com>
2020-02-25 15:01:38 +0900

    追加：ナビゲーションバーにRegister/Loginを追加

--- project4/ThreeLineDiary/views.py
+++ project4/ThreeLineDiary/views.py
@@ -1,7 +1,14 @@
 from django.views import generic
+from django.contrib.auth.views import LoginView
 from .models import Post
+from .forms import LoginForm
 
 
 class IndexView(generic.ListView):
     model = Post
 
+
+class Login(LoginView):
+    """ログインページ"""
+    form_class = LoginForm
+    template_name = '/login.html'
